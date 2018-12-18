123e974e043ac2f00c4ccf0278a0d2fcb7c97903
Veronica Bustabad <vbustabad@gmail.com>
2018-12-18 18:55:22 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -10,6 +10,7 @@ export default quotesReducer(state = [], action) => {
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
+<<<<<<< HEAD
     case 'UPVOTE_QUOTE':
       let currentQuote = initialState.filter(
         quote => quote.id === action.quoteId
@@ -29,6 +30,33 @@ export default quotesReducer(state = [], action) => {
         } else {
           return initialState;
         }
+=======
+<<<<<<< HEAD
+    //case 'UPVOTE_QUOTE':
+
+    //case 'DOWNVOTE_QUOTE':
+=======
+    case 'UPVOTE_QUOTE':
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+
+      case 'DOWNVOTE_QUOTE':
+        let existingAuthor = state.filter(
+          author => author.authorName === action.book.authorName
+        );
+        if (existingAuthor.length > 0) {
+          return state;
+        } else {
+          return [...state, { authorName: action.book.authorName, id: uuid() }];
+        }
+>>>>>>> 916bf6025f11640082aaedeac69ea65dbed31f83
+>>>>>>> ea7091055ca6bd5157e3bf1fb42e2b3d61bcb008
 
   default:
     return state;
